cdd4128ee0e193561b20a35d707fad9ec6ce3064
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-23 13:04:41 +0000

    turned the imageinto a component and passed props

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,12 @@
 import "../style/note.css";
+import NoteImage from "./NoteImage";
 
 function Note(props) {
   return (
     <div id="noteContainer" className={props.class}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <img src={props.img} alt="image" />
+      <NoteImage img={props.img} />
     </div>
   );
 }
---
+++ src/components/NoteImage.jsx
@@ -0,0 +1,7 @@
+import "../style/note.css";
+
+function NoteImage(props) {
+  return <img src={props.img} alt="image" />;
+}
+
+export default NoteImage;
