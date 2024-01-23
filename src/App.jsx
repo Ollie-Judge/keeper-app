735bb2619d5e131851672c9b3aa369e11b9ce5fc
Ollie-Judge <ollie.judge@hotmail.com>
2024-01-23 12:34:44 +0000

    added custom props class

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
       <Header />
       <Note
+        class="first"
         title="First Title"
         content="This is the first content"
         img="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.dyG9YY1J0V58F-e3hUH8rwHaFj%26pid%3DApi&f=1&ipt=177fbf06add3e3dc50b4fd71f45d540ec8c79507d86267af3af76cc84ddc4045&ipo=images"
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import "../style/note.css";
 
 function Note(props) {
   return (
-    <div id="noteContainer">
+    <div id="noteContainer" className={props.class}>
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <img src={props.img} alt="image" />
